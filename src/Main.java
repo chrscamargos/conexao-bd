a5e103ab7f66e96dccb1c92ee96a7e5ce710471c
chrscamargos <christianrodriguesdc2501@gmail.com>
2026-05-19 08:02:41 -0300

    aula de conexão de Java com Banco

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,70 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+void main(){
+
+//    janela
+    JFrame tela = new JFrame();
+    tela.setSize(500, 400);
+    tela.setLayout(null);
+
+//  --------------------------------
+
+//    label do nome
+    JLabel labelNome = new JLabel("Nome");
+    labelNome.setBounds(20, 50, 150, 40);
+    tela.add(labelNome);
+
+//    campo de texto do nome
+    JTextField nome = new JTextField();
+    nome.setBounds(20, 80, 250, 40);
+    tela.add(nome);
+
+//    --------------------------------------
+
+//    label do email
+    JLabel labelEmail = new JLabel("E-mail");
+    labelEmail.setBounds(20, 120, 150, 40);
+    tela.add(labelEmail);
+
+//    campo de texto do email
+    JTextField email = new JTextField();
+    email.setBounds(20, 150, 250, 40);
+    tela.add(email);
+
+//    ------------------------------------------------------
+
+//    botão
+    JButton enviar = new JButton("Enviar");
+    enviar.setBounds(80, 200, 150, 40 );
+    tela.add(enviar);
+
+    enviar.addActionListener(e -> {
+        String sql = "INSERT INTO usuario(nome, email) VALUES (?, ?)";
+        String nomeCompleto = nome.getText();
+        String emailUser = email.getText();
+
+        try {
+//            conexao com o banco
+            Connection conexao =  Conexao.conectar();
+            PreparedStatement ps = conexao.prepareStatement(sql);
+
+//            PreparedStatement ps = Conexao.conectar().prepareStatement(sql);
+
+            ps.setString(1, nomeCompleto);
+            ps.setString(2, emailUser);
+
+            ps.executeUpdate();
+
+            JOptionPane.showMessageDialog(null, "Usuário salvo com sucesso");
+            ps.close();
+
+
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    });
+
+    tela.setVisible(true);
 }
